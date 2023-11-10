fee042a77a983accb8a1c4ea0ec7ff8833674c0d
Timmy_mac <timmy@Timmy-macui-Macmini.local>
2023-11-10 10:48:58 +0900

    Add : 초기 세팅

---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+import { User } from "src/user/entities/user.entity";
+import { UserModule } from "src/user/user.module";
+
+export const ENTITIES = [User];
+
+export const MODULES = [UserModule];
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { HttpException, ValidationPipe, VersioningType } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory(errors) {
+        console.log(JSON.stringify(errors[0]));
+
+        return new HttpException(
+          {
+            id: `Out.of.control.error`,
+            message: "오류가 발생하였습니다. 고객센터로 문의해주세요.",
+          },
+          415,
+        );
+      },
+    }),
+  );
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.enableCors();
+
+  await app.listen(3005);
 }
 bootstrap();
